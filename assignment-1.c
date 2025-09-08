216f6c80136f5c264ff42b86398271126a0600da
reuben <reubenjstanley@gmail.com>
2025-09-08 16:50:12 +0800

    matrix is stored as 1d, contiguous in memory

--- assignment-1.c
+++ assignment-1.c
@@ -3,68 +3,111 @@
 #include <omp.h>
 #include <time.h>
 
-float** generate_random_matrix(int W, int H, int is_random) {
-    float** matrix = malloc(sizeof(float *)*W);
-    for (int i=0; i<W; i++) {
-        matrix[i] = malloc(sizeof(float)*H);
+float** generate_matrix_old(int m, int n, int is_random) {
+    srand(time(NULL));
+
+    // cache line is 64 bytes 
+    float **matrix = (float **)malloc(sizeof(float *) * m); // treat this array as one of int pointers
+                                                            // Problems:
+                                                            // m pointers defined represtenting m contiguous blocks of memory
+                                                            // malloc sends to heap, so no guarantee rows stored together 
+                                                            // 64 / 4 = 16 floats per cache line: potentially redundant memory in cache
+    
+    for (int i=0; i<m; i++) {
+        matrix[i] = malloc(sizeof(float) * n); 
     }
-    for (int i=0; i<W; i++) {
-        for (int j=0; j<H; j++) {
+
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
             if (is_random) {
-                matrix[i][j] = (float)rand()/RAND_MAX;
+            matrix[i][j] = (float)rand() / RAND_MAX;
             }
             else {
                 matrix[i][j] = 0.0;
             }
         }
     }
+
+    return matrix;
+
+}
+
+float* generate_random_matrix(int W, int H, int is_random) {
+    float* matrix = malloc(sizeof(float)*W*H);
+ 
+    for (int i=0; i<W*H; i++) {
+        if (is_random) {
+            matrix[i] = (float)rand()/RAND_MAX;
+        }
+        else {
+            matrix[i] = 0.0;            
+        }
+    }
     return matrix;
 }
 
-float** apply_padding(float** A, int aW, int aH, int pW, int pH) {
+float* apply_padding(
+    const float *A,   // input matrix (row-major, size aW x aH)
+    int aW, int aH,   // input dimensions: rows (aW) and cols (aH)
+    int pW, int pH   // total padding to add to rows and cols) 
+) {
     // Apply padding of {0} to pW rows & pH cols of the matrix A
 
-    float** padded_matrix = malloc(((aW+pW)*sizeof(float *)));
-    for (int i=0; i<aW+pW; i++) {
-        padded_matrix[i] = malloc((aH+pH)*sizeof(float));
-    }
-    int pW_start = pW/2;
-    int pH_start = pH/2;
+    if (aW <= 0 || aH <= 0 || pW < 0 || pH < 0) return NULL;
 
-    for (int i=0; i<(aW+pW); i++) {
-        for (int j=0; j<(aH+pH); j++) {
-            if ((i <= pW_start-1) || (i > aW+pW_start-1)) {
-                padded_matrix[i][j] = 0.0;
-            }
-        
-            else if ((j <= pH_start-1) || (j > aH+pH_start-1)) { // remember else if -> otherwise condition is missed
-                padded_matrix[i][j] = 0.0;
+    int newW = aW + pW; // new number of rows
+    int newH = aH + pH; // new number of columns
 
-            }
-            else {
-                padded_matrix[i][j] = A[i-pW_start][j-pH_start];
-            }
-            
+    int total = newW * newH;
+
+    float* padded_matrix = malloc(total*sizeof(float));
+
+
+    for (int i=0; i<total; i++) {padded_matrix[i] = 0.0;}
+
+    int p_top = pW/2; // # padded rows before first original row
+    int p_left = pH/2; // # padded rows before first original col 
+
+    for (int i=0; i<aW; ++i) {
+        for (int j=0; j<aH; ++j) {
+            padded_matrix[(i+p_top)*newH+j+p_left] = A[i*aH+j];
         }
+        
+            
     }
     return padded_matrix;
 }
 
-void print_matrix(float** A, int aW, int aH) {
-    for (int i=0; i<aW; i++) {
-        for (int j=0; j<aH; j++) {
-            if (j%aH == 0) {
-                printf("[");
-            }
-            printf(" %f ", A[i][j]);
-            if ((j+1) % aH == 0) {
-                printf("]\n");
-            }
+
+void print_matrix(float* A, int W, int H) {
+    for (int i=0; i<W*H; i++) {
+        if (i%H == 0) {
+            printf("[");
+
         }
+
+        printf(" %f ", A[i]);
+
+        if ((i+1) % H == 0) {
+            printf("]\n");
+        }
+        
     }
 }
 
-float dot_product(float** A, float** B, int aW, int aH, int bW, int bH) {
+float* flatten_matrix(float** A, int W, int H) {
+    float* flat_matrix = malloc(sizeof(float)*H*W);
+
+    for (int i=0; i<W; i++) {
+        for (int j=0; j<H; j++) {
+            flat_matrix[(i*H)+j] = A[i][j];
+        }
+    }
+    // print_matrix(flat_matrix, W, H);
+    return flat_matrix;
+}
+
+float dot_product(float* A, float* B, int aW, int aH, int bW, int bH) {
     // Apply the dot product to 2 equal matricies, outputting the sum of elements
 
     float sum = 0.0;
@@ -73,10 +116,10 @@ float dot_product(float** A, float** B, int aW, int aH, int bW, int bH) {
         return 0.0;
     }
 
-    // #pragma omp collapse(2) reduction(+:sum)
+    #pragma omp collapse(2) reduction(+:sum)
     for (int i=0; i<aW; i++) {
         for (int j=0; j<aH; j++) {
-            sum += A[i][j]*B[i][j]; // potential for reduction & collapse here... possibly depends on kernel size
+            sum += A[i*aW+j]*B[i*bW+j]; // potential for reduction & collapse here... possibly depends on kernel size
         }
     }
     return sum;
@@ -97,29 +140,38 @@ void conv2d(
     int pW = kW - 1;
     int pH = kH - 1;
 
-    float** padded_matrix = apply_padding(f, W, H, pW, pH);
-
+    float* flat_matrix = flatten_matrix(f, W, H);  
     free(f); // no longer required
 
-    float** output_matrix = generate_random_matrix(W, H, 0);
-    float** temp_matrix = generate_random_matrix(kW, kH, 0); 
+    float* flat_kernel = flatten_matrix(g, kW, kH);
+    free(g);
 
+    float* padded_matrix = apply_padding(flat_matrix, W, H, pW, pH);
+    free(flat_matrix);
+
+    // print_matrix(padded_matrix, W+pW, H+pH);
+    
+
+    float* output_matrix = generate_random_matrix(W, H, 0);
+    float* temp_matrix = generate_random_matrix(kW, kH, 0); 
+
+    printf("Starting Convultion,\n");
     double start = omp_get_wtime();
     
     #pragma omp parallel for //firstprivate(temp_matrix)
     for (int i=0; i<W; i++) {
         for (int j=0; j<H; j++) {
             
-            // #pragma omp collapse(2)
+            #pragma omp collapse(2)
             for (int kh=0; kh<kH; kh++) {
                 for (int kw=0; kw<kW; kw++) {
-                    temp_matrix[kw][kh] = padded_matrix[i+kw][j+kh];
+                    temp_matrix[(kh*kW)+kw] = padded_matrix[((i+kw)*H)+j+kh];
                 }
             }
             // #pragma omp task 
             {
-            float dp_val = dot_product(temp_matrix, g, kW, kH, kW, kH);
-            output_matrix[i][j] = dp_val;
+            float dp_val = dot_product(temp_matrix, flat_kernel, kW, kH, kW, kH);
+            output_matrix[i*H+j] = dp_val;
             }
 
         }
@@ -134,16 +186,16 @@ void conv2d(
 
 int main() {
     srand(time(NULL));
-    int W = 5000;
-    int H = 5000;
-    int kW = 5;
-    int kH = 5;
-
+    int W = 1000;
+    int H = 1000;
+    int kW = 500;
+    int kH = 500;
+    
     float** output = malloc(0*sizeof(float *)); // TODO: what is this?
 
-    float** f = generate_random_matrix(W, H, 1);
-    float** g = generate_random_matrix(kW, kH, 1);
-    
+    float** f = generate_matrix_old(W, H, 1);
+    float** g = generate_matrix_old(kW, kH, 1);
+
     conv2d(f, H, W, g, kH, kW, output);
     return 0;
 }
